982b448f15bba0449424816c63194aa0b640bd45
Mario <mario@mario-PC.(none)>
2013-10-15 19:15:02 +0200

    añadido privileges.sh por si acaso

---
+++ privileges.sh
@@ -0,0 +1,24 @@
+sudo chmod 0775 ../propon
+sudo chmod 0775 application assets css js system user_guide work
+sudo chmod 0777 captcha
+sudo chmod 0775 application/cache application/config application/controllers application/core application/errors application/hooks application/language application/libraries application/logs application/models application/third_party application/views
+sudo chmod 0775 application/controllers/api application/controllers/cms application/controllers/analytics
+sudo chmod 0775 application/views/analytics application/views/cms
+sudo chmod 0775 application/language/english
+sudo chmod 0775 assets/icons assets/photos assets/fonts
+sudo chmod 0777 assets/icons/sociality assets/icons/category assets/icons/actions assets/icons/flags
+sudo chmod 0777 assets/photos/proposal assets/photos/user
+sudo chmod 0775 js/libraries js/cms js/analytics
+sudo chmod 0775 js/libraries/DataTables js/libraries/carouFredSel js/libraries/fancybox
+sudo chmod 0775 js/libraries/DataTables/extras js/libraries/DataTables/media js/libraries/DataTables/scripts
+sudo chmod 0775 js/libraries/DataTables/media/css js/libraries/DataTables/media/images js/libraries/DataTables/media/js js/libraries/DataTables/media/src
+sudo chmod 0775 js/libraries/DataTables/media/src/api js/libraries/DataTables/media/src/core js/libraries/DataTables/media/src/ext js/libraries/DataTables/media/src/model
+sudo chmod 0775 js/libraries/carouFredSel/helper-plugins js/libraries/carouFredSel/images
+sudo chmod 0775 js/libraries/fancybox/lib js/libraries/fancybox/source
+sudo chmod 0775 js/libraries/fancybox/source/helpers
+sudo chmod 0775 system/core system/database system/helpers system/language system/libraries
+sudo chmod 0775 system/database/drivers
+sudo chmod 0775 system/database/drivers/mysql
+sudo chmod 0775 system/language/english system/language/spanish
+sudo chmod 0775 system/libraries/Cache system/libraries/javascript
+sudo chmod 0775 system/libraries/Cache/drivers
